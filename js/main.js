bada9f17e630e962a05fb87f8c4f4f023e2bdb6d
sanfranciscodeasis <mireyasantiago105@gmail.com>
2017-07-18 00:19:13 -0500

    cambios en el diseño

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
-$('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 300
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
-    }
-  );
+(function(){
 
+var desencadenar = $('.menu');
+var cubrir = $('#cubrir');
+var altenarElemento = $('.desplazar');
+var estaCerrado = false;
 
+var cargarPagina = function(){
+  desencadenar.click(menu_deslizado);
+  altenarElemento.click(alternar);
+};
 
-      // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-      $('.modal').modal();
+var menu_deslizado = function(){
+
+  if (estaCerrado == true) {
+    cubrir.hide();/*esconde*/
+  } else {
+    cubrir.show();/*muestra*/
+  }
+};
+
+var alternar = function(){
+  $('#wrapper').toggleClass('cambiado');
+/*toogleClass: alterna entre agregar y quitar uno o más nombres de clase de los elementos seleccionados.*/
+}
+
+$(document).ready(cargarPagina);
+
+})();
