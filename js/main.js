abbe4746917e1e4f7812e9a02821862692e9f50e
sanfranciscodeasis <mireyasantiago105@gmail.com>
2017-07-25 23:03:16 -0500

    cambios en el menu

--- js/main.js
+++ js/main.js
@@ -1,29 +1,25 @@
-(function(){
+$(document).ready(function(){
 
-var desencadenar = $('.menu');
-var cubrir = $('#cubrir');
-var altenarElemento = $('.desplazar');
-var estaCerrado = false;
 
-var cargarPagina = function(){
-  desencadenar.click(menu_deslizado);
-  altenarElemento.click(alternar);
-};
+    $('.show-menu').on('click', function(){
 
-var menu_deslizado = function(){
+        $('.content-menu').slideToggle();
+    })
+/*El método resize () desencadena el evento
+redimensionar o asigna una función para ejecutarse cuando
+se produce un evento de cambio de tamaño.*/
+    $(window).resize(function(){
+        /*el metodo heigth:establece o devuelve la altura de los
+        elementos seleccionados.*/
 
-  if (estaCerrado == true) {
-    cubrir.hide();/*esconde*/
-  } else {
-    cubrir.show();/*muestra*/
-  }
-};
 
-var alternar = function(){
-  $('#wrapper').toggleClass('cambiado');
-/*toogleClass: alterna entre agregar y quitar uno o más nombres de clase de los elementos seleccionados.*/
-}
+        if($(window).width() > 983){
+            $('.content-menu').show();
 
-$(document).ready(cargarPagina);
+        }else{
+            $('.content-menu').hide();
+        }
 
-})();
+    })
+
+})
