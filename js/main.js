6c3b40df550cead6735bbebcbd39b8f34d89802d
sanfranciscodeasis <mireyasantiago105@gmail.com>
2017-07-25 23:40:49 -0500

    cambiando la funcionalidad

--- js/main.js
+++ js/main.js
@@ -23,3 +23,35 @@ se produce un evento de cambio de tamaño.*/
     })
 
 })
+
+/*
+(function(){
+
+var desencadenar = $('.menu');
+var cubrir = $('#cubrir');
+var altenarElemento = $('.desplazar');
+var estaCerrado = false;
+
+var cargarPagina = function(){
+  desencadenar.click(menu_deslizado);
+  altenarElemento.click(alternar);
+};
+
+var menu_deslizado = function(){
+
+  if (estaCerrado == true) {
+    cubrir.hide();/*esconde*/
+  /*} else {
+    cubrir.show();/*muestra*/
+/*  }
+};
+
+var alternar = function(){
+  $('#wrapper').toggleClass('cambiado');
+/*toogleClass: alterna entre agregar y quitar uno o más nombres de clase de los elementos seleccionados.*/
+/*}
+
+$(document).ready(cargarPagina);
+
+})();
+*/
